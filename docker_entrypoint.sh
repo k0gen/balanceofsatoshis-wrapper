4142b71b273d964daa3d11a48ac5783e1c5bda7c
Mariusz Kogen <k0gen@pm.me>
2022-06-09 17:34:27 +0200

    fix for ui link and manifest changes

--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -11,4 +11,4 @@ mv /credentials.json /home/bos/.bos/embassy/credentials.json
 chmod a+x /home/bos/.bos/embassy/credentials.json
 chmod 640 /mnt/lnd/admin.macaroon
 echo 'export BOS_DEFAULT_SAVED_NODE=embassy' >> /home/bos/.bashrc
-wetty --title Balance of Satoshi for Embassy
+wetty -b / --title Balance of Satoshi for Embassy
